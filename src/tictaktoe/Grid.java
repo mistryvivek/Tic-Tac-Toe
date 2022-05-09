b7477cab41a7ee13612781f6a8ffd0409424caad
Vivek Mistry <mistry.vivek03@gmail.com>
2022-05-09 15:30:58 +0100

    Convience method in order to get a row read

--- src/tictaktoe/Grid.java
+++ src/tictaktoe/Grid.java
@@ -4,8 +4,10 @@ public class Grid{
     Square [][] grid;
     
     class Square{
+        //Says whether is a nought or a cross.
         String val; 
 
+        //Setter methods.
         public void set_nought(){
             val = "O";
         }
@@ -14,10 +16,23 @@ public class Grid{
             val = "X";
         }
 
+        //Getter methods.
         public String get_val(){
             return val;
         }
     }
 
+    public void displayBoard(){
+        String empty_line = "--------";
+        String top_line = 
+
+    }
+
+    //Read a row and get it in the correct format.
+    private String format(i){
+        return grid[i][0].get_val() + "|" + grid[i][1].get_val() + "|" + grid[i][2].get_val();
+    }
+
+    
 
 }
