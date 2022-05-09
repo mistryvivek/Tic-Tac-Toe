a9fd18e499f25a098bfb79527a1db46599343aa8
Vivek Mistry <mistry.vivek03@gmail.com>
2022-05-09 15:09:49 +0100

    Create a nested class within the grid to control the value in each element

---
+++ src/Grid.java
@@ -0,0 +1,20 @@
+public class Grid{
+    
+    class Sqaure{
+        String val; 
+
+        public void set_nought(){
+            val = "O";
+        }
+
+        public void set_cross(){
+            val = "X";
+        }
+
+        public String get_val(){
+            return val;
+        }
+
+
+    }
+}
