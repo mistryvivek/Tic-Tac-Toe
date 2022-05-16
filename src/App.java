2d9b68d0483ddaf18bacbc9c9818c3a586c654cb
Vivek Mistry <mistry.vivek03@gmail.com>
2022-05-16 14:52:43 +0100

    Testing whether the bot blocks my move when selecting hard mode.

--- src/App.java
+++ src/App.java
@@ -1,21 +1,26 @@
 import java.util.Arrays;
-import java.util.List;
 
 import tictaktoe.*;
 
 public class App {
     //Using this class for testing 
     public static void main(String[] args) throws Exception {
-        Grid game = new Grid();
+        Grid game = new Grid("hard");
         System.out.println(game.displayBoard());
 
         //Test have been carried out to check if all combinations work.
+        System.out.print(game.usersTurn(Arrays.asList(new Integer[]{0,1})));
+        System.out.print(game.botsTurn());
         System.out.print(game.usersTurn(Arrays.asList(new Integer[]{0,2})));
-        System.out.print(game.usersTurn(Arrays.asList(new Integer[]{1,1})));
-        System.out.print(game.usersTurn(Arrays.asList(new Integer[]{2,0})));
+        System.out.print(game.botsTurn());
+
                
 
         /*
+        System.out.print(game.usersTurn(Arrays.asList(new Integer[]{1,0})));
+        System.out.print(game.usersTurn(Arrays.asList(new Integer[]{1,1})));
+        System.out.print(game.usersTurn(Arrays.asList(new Integer[]{1,2})));
+
         System.out.print(game.usersTurn(Arrays.asList(new Integer[]{0,0})));
         System.out.print(game.usersTurn(Arrays.asList(new Integer[]{0,1})));
         System.out.print(game.usersTurn(Arrays.asList(new Integer[]{0,2})));
@@ -37,10 +42,14 @@ public class App {
         System.out.print(game.usersTurn(Arrays.asList(new Integer[]{2,1})));
         System.out.print(game.usersTurn(Arrays.asList(new Integer[]{2,2})));
         
-        
+        System.out.print(game.usersTurn(Arrays.asList(new Integer[]{0,2})));
+        System.out.print(game.usersTurn(Arrays.asList(new Integer[]{1,1})));
+        System.out.print(game.usersTurn(Arrays.asList(new Integer[]{2,0})));
 
         
         */
+
+        
 
         System.out.println(game.checkWin());
         
